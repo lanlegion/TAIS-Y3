d323cffcc2905d0422567e242c1fd0bffd654ce0
Burbon13 <razvanroatis91@gmail.com>
2020-04-13 20:38:46 +0300

    Added food on the map

--- app/simulation.js
+++ app/simulation.js
@@ -8,14 +8,22 @@ const COLORS = {
 };
 
 const SIZES = {
-  HOME_WIDTH: 7,
-  HOME_HEIGHT: 7,
-  ANT_WIDTH: 2,
-  ANT_HEIGHT: 2,
+  HOME: {
+    X: 7,
+    Y: 7,
+  },
+  ANT: {
+    X: 2,
+    Y: 2,
+  },
+  FOOD: {
+    X: 1,
+    Y: 1,
+  }
 };
 
 class Simulation {
-  constructor(width, height, numberOfColonies, antsPerColony, gameSpeed) {
+  constructor(width, height, numberOfColonies, antsPerColony, gameSpeed, foodStacksCount, foodStackSize) {
     console.log(`Initializing Simulation width=${width} height=${height} 
     numberOfColonies=${numberOfColonies} antPerColony=${antsPerColony}`);
 
@@ -24,23 +32,39 @@ class Simulation {
     this.numberOfColonies = numberOfColonies;
     this.antPerColony = antsPerColony;
     this.gameSpeed = gameSpeed;
+    this.foodStacksCount = foodStacksCount;
+    this.foodStackSize = foodStackSize;
 
-    this._initHome();
+    this._initCells();
+    this._initHomes();
     this._initAnts();
     this._initColoniesColors();
+    this._initFoodStacks();
 
     console.log('Simulation initialization complete!');
   }
 
-  _initHome() {
+  _initHomes() {
     console.log('Initializing homes');
     this.homes = [];
     for (let colony = 0; colony < this.numberOfColonies; colony++) {
-      const xHome = 1 + Math.random() * this.width;
-      const yHome = 1 + Math.random() * this.height;
+      const xHome = 1 + int(Math.random() * this.width);
+      const yHome = 1 + int(Math.random() * this.height);
       const newHome = new Cell(xHome, yHome);
       newHome.type = CellType.HOME;
       this.homes.push(newHome);
+    }
+  }
+
+  _initCells() {
+    console.log('Initializing cells');
+    this.cells = [];
+    for (let x = 0; x < this.width; x++) {
+      let row = [];
+      for (let y = 0; y < this.height; y++) {
+        row.push(new Cell(x, y));
+      }
+      this.cells.push(row);
     }
   }
 
@@ -64,6 +88,22 @@ class Simulation {
     }
   }
 
+  _initFoodStacks() {
+    console.log('Initializing food stacks');
+    this.foods = [];
+    for (let food = 0; food < this.foodStacksCount; food++) {
+      const xFood = 1 + int(Math.random() * this.width);
+      const yFood = 1 + int(Math.random() * this.height);
+      for (let x = xFood; x < xFood + this.foodStackSize && x < this.width; x++) {
+        for (let y = yFood; y < yFood + this.foodStackSize && y < this.height; y++) {
+          const currentCell = this.cells[x][y];
+          currentCell.type = CellType.FOOD;
+          this.foods.push(currentCell);
+        }
+      }
+    }
+  }
+
   run() {
     this.ants.forEach(colony => {
       colony.forEach(ant => {
@@ -73,14 +113,12 @@ class Simulation {
   }
 
   draw() {
-    console.log('drawing');
-
     background(COLORS.BACKGROUND_COLOR);
     noStroke();
 
     this.homes.forEach((home, index) => {
       fill(this.colonyColors[index]);
-      ellipse(home.x, home.y, SIZES.HOME_WIDTH, SIZES.HOME_HEIGHT);
+      ellipse(home.x, home.y, SIZES.HOME.X, SIZES.HOME.Y);
     });
 
     this.ants.forEach((colony, index) => {
@@ -92,8 +130,13 @@ class Simulation {
           antColor = COLORS.ANT_WITH_FOOD;
         }
         fill(antColor);
-        rect(ant.x, ant.y, SIZES.ANT_WIDTH, SIZES.ANT_HEIGHT);
+        rect(ant.x, ant.y, SIZES.ANT.X, SIZES.ANT.Y);
       })
-    })
+    });
+
+    fill(COLORS.FOOD_COLOR);
+    this.foods.forEach(foodCell => {
+      rect(foodCell.x, foodCell.y, SIZES.FOOD.X, SIZES.FOOD.Y);
+    });
   }
 }
--- app/sketch.js
+++ app/sketch.js
@@ -1,17 +1,19 @@
 const WIDTH = 1000;
 const HEIGHT = 600;
-const FPS = 30;
-const NUMBER_OF_COLONIES = 3; // Max 5
-const ANTS_PER_COLONY = 50;
-const GAME_SPEED = 0.3;
+const FPS = 60;
+const NUMBER_OF_COLONIES = 5; // Max 5
+const ANTS_PER_COLONY = 200;
+const FOOD_STACKS = 20;
+const FOOD_STACK_SIZE = 10;
+const GAME_SPEED = 0.4;
 
 let simulation;
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  // frameRate(FPS);
+  frameRate(FPS);
 
-  simulation = new Simulation(WIDTH, HEIGHT, NUMBER_OF_COLONIES, ANTS_PER_COLONY, GAME_SPEED);
+  simulation = new Simulation(WIDTH, HEIGHT, NUMBER_OF_COLONIES, ANTS_PER_COLONY, GAME_SPEED, FOOD_STACKS, FOOD_STACK_SIZE);
 }
 
 function draw() {
